eb8de27a52926554ca04967c0266636a2cff0d1e
Aakashtyagi9889 <aakashtyagi9889@gmail.com>
2026-06-07 18:31:59 +0530

    Updated

--- practise/File Handling.py
+++ practise/File Handling.py
@@ -1,7 +1,24 @@
-a = int(input("Enter a number"))
-b = int(input("Enter b number"))
+# a = int(input("Enter a number"))
+# b = int(input("Enter b number"))
 
-try:
-    print("division", a // b)
-except SyntaxError as e:
-    print(" error", e)
+# try:
+#     print("division", a // b)
+# except SyntaxError as e:
+#     print(" error", e)
+
+
+# a= input(" Enter First Movie ")
+# b= input(" Enter Second Movie ")
+# c= input(" Enter Third Movie ")
+
+# li = []
+# li.append(a)
+# li.append(b)
+# li.append(c)
+
+# print(li)
+
+
+# grade = ["c" ,"b" , "a" , "d" , "e"]
+# grade = grade.sort()
+# print(grade)
