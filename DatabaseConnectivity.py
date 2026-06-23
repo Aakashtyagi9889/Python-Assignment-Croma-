b15077646e8390046b88436baf40b2a3f0a4d68d
Aakashtyagi9889 <aakashtyagi9889@gmail.com>
2026-06-23 10:12:23 +0530

    Practise for database connectivity

--- DatabaseConnectivity.py
+++ DatabaseConnectivity.py
@@ -1,53 +1,91 @@
+# import mysql.connector;
+# #  In this library we have the method connect which help to connect the python to mysql ==> connect()
+
+# conn = mysql.connector.connect(
+#     host = 'localhost',
+#     port = '3306',
+#     username = 'root',
+#     password = 'aaka0505',
+#     database = 'cromacampus'
+#     )
+# print(conn)
+
+# # cursor() is the method which helps to write the query in python to database
+# cur = conn.cursor()
+
+
+# #==========================================IT IS MANDOTORY ===========================================
+# """
+# sql  = "CREATE DATABASE cromaCampus"
+# cur.execute(sql)
+
+
+# sql = '''
+# Create TABLE student(
+# sid int primary key auto_increment,
+# sname varchar(30),
+# sadd text,
+# email text
+# )
+# '''
+# cur.execute(sql)
+
+# INSERT VALUES---
+# sql =  "insert into student values(101 , 'Aakash' , 'Ghazaiabad' , 'a.gmail.com')"  
+# cur.execute(sql)
+# print(cur.rowcount)
+# conn.commit()
+# """
+
+
+
+
+
+
+# sql  ="select * from student"
+# print(cur.execute(sql))
+
+
+
+
+
+
 import mysql.connector;
-#  In this library we have the method connect which help to connect the python to mysql ==> connect()
-
 conn = mysql.connector.connect(
-    host = 'localhost',
-    port = '3306',
-    username = 'root',
-    password = 'aaka0505',
-    database = 'cromacampus'
+    host = "localhost",
+    password = "aaka0505",
+    port = "3306",
+    username = "root",
+    database = "db_connectivity"
     )
-print(conn)
-
-# cursor() is the method which helps to write the query in python to database
 cur = conn.cursor()
 
 
-#==========================================IT IS MANDOTORY ===========================================
-"""
-sql  = "CREATE DATABASE cromaCampus"
-cur.execute(sql)
 
 
-sql = '''
-Create TABLE student(
-sid int primary key auto_increment,
-sname varchar(30),
-sadd text,
-email text
-)
-'''
-cur.execute(sql)
 
-INSERT VALUES---
-sql =  "insert into student values(101 , 'Aakash' , 'Ghazaiabad' , 'a.gmail.com')"  
-cur.execute(sql)
-print(cur.rowcount)
-conn.commit()
+sql = """insert into student values
+(101 , 'Aakash' , 24000.26),
+(102 , 'avi ' , 25000.26),
+(102 , 'Abhay Tyagi ' , 24000.26)
+
 """
 
+sql  =  "select * from student"
+cur.execute(sql)
+data =  cur.fetchall()
+
+
+for a in data:
+    print("Student Id : " , a[0])
+    print("Student Name : " ,a[1])
+    print("Student Salary : " ,a[2])
+    print("--------------------------")
 
 
 
-
-
-sql  ="select * from student"
-print(cur.execute(sql))
-
-
-
-
+sql  =  "drop database db_connectivity"
+cur.execute(sql)
 
 
 
